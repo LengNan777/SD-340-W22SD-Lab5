b04f90b66fd9e8b2a43e6e24b5da9cf0a0c5ee59
LengNan777 <L2452520160@gmail.com>
2022-09-26 18:17:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+string path = @"C:\Users\VULCAN\Desktop";
+
+Console.WriteLine($"Current directory is '{Environment.CurrentDirectory}'");
+Directory.SetCurrentDirectory(path);
+Console.WriteLine($"Current directory is '{Environment.CurrentDirectory}'");
+
+Console.WriteLine(ReplacePeriod(path));
+
+string ReplacePeriod(string path)
+{
+    string content;
+    string editedContent;
+
+    try
+    {
+        using (StreamReader sr = File.OpenText($"{path}\\theMachineStops.txt"))
+        {
+            content = sr.ReadToEnd();
+            editedContent = content.Replace(@".", "STOP");
+        }
+
+        using (StreamWriter sw = new StreamWriter("TelegramCopy.txt"))
+        {
+            sw.WriteLine(editedContent);
+        }
+        return "It works!";
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    return "No content in file.";
+}
